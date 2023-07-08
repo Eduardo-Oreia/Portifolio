0681da994e89325baf74a75977ecbf5a401fad80
Carlos Eduardo Batista <carloswe3st@gmail.com>
2023-07-08 13:04:45 -0300

    portifolio atualizado

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,3 +3,22 @@ const btnTheme = document.getElementById("moon-btn")
 btnTheme.addEventListener("click", () => {
     document.body.classList.toggle("light")
 })
+
+
+const sr =  ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 1700,
+    delay: 200,
+    reset: true,
+})
+
+sr.reveal('.presentation')
+sr.reveal('.title')
+sr.reveal('.icons-container', {origin: 'bottom'})
+sr.reveal('.roll-down', {origin: 'left'})
+sr.reveal('.info-about', {origin: 'right'})
+sr.reveal('.my-photo-about', {origin: 'left'})
+sr.reveal('.skills-column', {origin: 'left'})
+sr.reveal('.quality-column', {origin: 'right'})
+sr.reveal('.footer', {origin: 'bottom'})
